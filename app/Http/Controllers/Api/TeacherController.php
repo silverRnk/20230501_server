55c979b95bae8a5b34c5a43a6833601cf0c6a3fd
pdbautista0 <patdi.bautista@gmail.com>
2023-05-25 20:17:02 +0800

    Added Implementing code for
    add and view

--- app/Http/Controllers/Api/TeacherController.php
+++ app/Http/Controllers/Api/TeacherController.php
@@ -4,20 +4,34 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AddTeacherRequest;
-use App\Http\Requests\AllTeachersRequest;
+use App\Http\Resources\TeachersCollection;
+use App\Http\Resources\ViewTeacherCollection;
 use Illuminate\Http\Request;
+use App\Models\Teacher;
 
 class TeacherController extends Controller
 {
-    public function all(AllTeachersRequest $request){
-        
+    public function all(){
+
+        return new TeachersCollection(
+            Teacher::query()
+            ->orderBy('id', 'desc')
+            ->paginate(10)
+        );
+
     }
 
     public function add(AddTeacherRequest $request){
+        //TODO
+        $data = $request->validated();
 
+        
     }
 
     public function view(string $id){
-
+        
+        return new ViewTeacherCollection(
+            Teacher::where('id', $id)->get()
+        );
     }
 }
