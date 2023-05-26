49c49acf8299454908da38a104f472c8364110bf
pdbautista0 <patdi.bautista@gmail.com>
2023-05-26 13:50:25 +0800

    Implementing add teacher controllers
    Implementing AddTeacher Request
    Implementing TeacherControllers Add Method

--- app/Http/Controllers/Api/TeacherController.php
+++ app/Http/Controllers/Api/TeacherController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\TeachersCollection;
 use App\Http\Resources\ViewTeacherCollection;
 use Illuminate\Http\Request;
 use App\Models\Teacher;
+use Illuminate\Support\Facades\Storage;
 
 class TeacherController extends Controller
 {
@@ -25,6 +26,32 @@ class TeacherController extends Controller
         //TODO
         $data = $request->validated();
 
+        //Create Row
+        $teacher = Teacher::create([
+            'name' => $data['first_name'].' '.$data['last_name'],
+            'gender' => $data['gender'],
+            'date_of_birth' => $data['date_of_birth'],
+            'religion' => $data['religion'],
+            'advisory_class' => $data['advisory_class'],
+            'email' => $data['email'],
+            'phone_no' => $data['phone_no'],
+            'password' => bcrypt($data['password'])
+        ]
+        );
+
+        //Get Image from request
+        $file = $data['profile_img'];
+    
+        //Rename Image
+        $file_name = $teacher->id.'.'.$file->extension();
+        
+        //Save Image to storage then get the file path
+        $img_path = Storage::putFileAs('teacher_profile_imgs', $file, $file_name);
+        //Add the file path to the current teacher
+        $teacher->profile_img = $img_path;
+        $teacher->save();
+        //return successful
+        return response(['message' => 'New Teacher is added successfully'], 201);
         
     }
 
--- app/Http/Requests/AddTeacherRequest.php
+++ app/Http/Requests/AddTeacherRequest.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-
+use Illuminate\Validation\Rules\Password;
 class AddTeacherRequest extends FormRequest
 {
     /**
@@ -23,7 +23,35 @@ class AddTeacherRequest extends FormRequest
     {
     
         return [
-            //TODO define validation rule
+            'first_name' => ['required', 'string'],
+            'last_name' => ['required', 'string'],
+            'addr_line1' => ['required', 'string'],
+            'addr_line2' => ['string', 'nullable', ],
+            'gender' => ['required','in:male,female'],
+            'date_of_birth' => ['required', 'date'],
+            'religion' => ['string', 'nullable'],
+            'email' => ['required', 'email',
+             'unique:teachers,email'],
+            'phone_no' => ['required', 'numeric'],
+            'advisory_class' => ['string', 'nullable'],
+            'profile_img' => ['file', 'image', 'max:1024', 'nullable'],
+            'password' => [Password::min(6), 'required', 'confirmed']
         ];
     }
+
+    public function messages(){
+
+        return [
+            '*.required' => 'Input is required',
+            'gender.in' => 'Input must be either male or female',
+            'email.unique' => 'Email already exists',
+            'email.required' => 'Input is required',
+            'password.min' => 'Input must be minimum of 6 character',
+            'password.confirmed' => 'Confirm your password',
+            'profile_img.max' => 'Maximum image size must be less than 1Mb',
+            'profile_img.image' => 'File must be an image',
+            'profile_img.file' => 'Uploaded must be a file and an image'
+        ];
+    }
+
 }
