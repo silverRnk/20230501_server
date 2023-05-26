7f426752fddbdf1564e48a8ea9c4e358f4c5fbff
pdbautista0 <patdi.bautista@gmail.com>
2023-05-26 20:07:37 +0800

    Resolving migration issue and added column
    Added admission_date col

--- database/migrations/2023_05_21_023302_create_teachers_table.php
+++ database/migrations/2023_05_21_023302_create_teachers_table.php
@@ -17,11 +17,14 @@ return new class extends Migration
             $table->enum('gender', ['male', 'female']);
             $table->date('date_of_birth');
             $table->string('religion')->nullable();
-            $table->string('advisory_class')->nullable();
+            $table->foreignId('advisory_class')
+            ->constrained('class_sections')
+            ->nullable();
             $table->string('profile_img')->nullable();
             $table->string('email');
             $table->string('phone_no');
             $table->string('password');
+            $table->date('admission_date');
             $table->timestamps();
         });
     }
